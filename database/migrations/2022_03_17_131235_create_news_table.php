c1deaaeb735b4ac09b3cc5d5828026f3149efd35
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-03-17 21:03:53 +0600

    added migration file

--- database/migrations/2022_03_17_131235_create_news_table.php
+++ database/migrations/2022_03_17_131235_create_news_table.php
@@ -15,6 +15,19 @@ class CreateNewsTable extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('title_bangla');
+            $table->string('sub_title');
+            $table->string('sub_title_bangla');
+            $table->text('details');
+            $table->text('details_bangla');
+            $table->string('ticker')->nullable();
+            $table->string('ticker_bangla')->nullable();
+            $table->string('keyword')->nullable();
+            $table->bigInteger('category_id');
+            $table->integer('order')->nullable();
+            $table->string('image')->nullable();
+            $table->string('type');
             $table->timestamps();
         });
     }
