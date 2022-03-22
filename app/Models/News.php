0a18378f2329086a38392d3bcf4dffaeca45b39a
muhaiminulIslamIkhlas <muhaiming2c@gmail.com>
2022-03-22 20:04:33 +0600

    Complete news details controller

--- app/Models/News.php
+++ app/Models/News.php
@@ -19,6 +19,11 @@ class News extends Model
         return $this->belongsTo('App\Models\Category', 'category_id');
     }
 
+    public function subCategory()
+    {
+        return $this->belongsTo('App\Models\Subcategory', 'sub_category_id');
+    }
+
     public function format()
     {
         return [
@@ -30,6 +35,25 @@ class News extends Model
             'time' => $this->created_at,
             'image' => $this->image,
             'type' => $this->type
+        ];
+    }
+
+    public function formatDetails()
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'sort_description' => $this->sort_description,
+            'order' => $this->order,
+            'category' => $this->category->name,
+            'sub_category' => $this->subCategory->name ?? '',
+            'time' => $this->created_at,
+            'image' => $this->image,
+            'type' => $this->type,
+            'details' => $this->details->details,
+            'ticker' => $this->details->ticker,
+            'representative' => $this->details->representative,
+            'keyword' => $this->details->keyword,
         ];
     }
 }
